934e0efc5266494d2353348ed2554a21c63d4083
Nikita Belov <nickibelov@yandex.ru>
2023-11-15 19:11:02 +0500

    оы срфтпуы

--- js/script.js
+++ js/script.js
@@ -1,49 +1,139 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var checkboxes = document.querySelectorAll('.custom-checkbox input[type="checkbox"]');
+    const checklistData = [
+        {
+            title: "PRE FLIGHT CHECKLIST",
+            items: [
+                { id: 1, label: "PARKING BRAKE", subtitle: "SET", typeItem: "standard" },
+                { id: 2, label: "BATTERY", subtitle: "GUARD CLOSED", typeItem: "standard" },
+                { id: 3, label: "STANDBY POWER", subtitle: "GUARD CLOSED", typeItem: "standard" }.
+            ]
+        },
+        {
+            title: "BEFORE START CHECKLIST",
+            items: [
+                { id: 4, label: "PARKING BRAKE", subtitle: "SET", typeItem: "standard" },
+                { id: 5, label: "BATTERY", typeItem: "normal" },
+                { id: 6, label: "STANDBY POWER", typeItem: "italic" }
+            ]
+        }
+    ];
 
-    checkboxes.forEach(function(checkbox){
-        checkbox.addEventListener('change', function() {
-            var blockItem = this.closest('.block-item__content__item');
-            if (this.checked) {
-                blockItem.classList.add('checked');
-            } else {
-                blockItem.classList.remove('checked');
+    // Функция для создания HTML-элементов чеклиста
+    function createChecklistElement(checklist) {
+        const checklistElement = document.createElement("div");
+        checklistElement.className = "block-item";
+
+        const titleElement = document.createElement("div");
+        titleElement.className = "block-item__title";
+        titleElement.textContent = checklist.title;
+        checklistElement.appendChild(titleElement);
+
+        const contentElement = document.createElement("div");
+        contentElement.className = "block-item__content";
+
+        checklist.items.forEach(item => {
+            const itemElement = document.createElement("div");
+            itemElement.className = "block-item__content__item";
+            itemElement.setAttribute("data-id-item-checklist", item.id);
+
+            const checkboxLabel = document.createElement("label");
+            if(item.typeItem !== 'italic') {
+                checkboxLabel.className = "custom-checkbox";
+                const checkboxInput = document.createElement("input");
+                checkboxInput.type = "checkbox";
+
+                checkboxInput.setAttribute("data-id", item.id);
+                checkboxLabel.appendChild(checkboxInput);
             }
-        });
-    });
+            checkboxLabel.innerHTML += `<span>${item.label}</span>`;
+            itemElement.appendChild(checkboxLabel);
 
-    const checklist = document.querySelectorAll('.custom-checkbox input[type="checkbox"]');
+            if (item.typeItem === "standard") {
+                const dottedElement = document.createElement("div");
+                dottedElement.className = "dotted";
+                itemElement.appendChild(dottedElement);
 
-    checklist.forEach(function(checkbox) {
-        checkbox.addEventListener('change', function(event) {
-            const itemId = event.target.dataset.id;
-            const isChecked = event.target.checked;
-            localStorage.setItem(itemId, isChecked);
-        });
-    });
-
-    // Загрузка состояний чекбоксов из local storage при загрузке страницы
-    function loadChecklistState() {
-        var checkboxes = document.querySelectorAll('.custom-checkbox input[type="checkbox"]');
-
-        checkboxes.forEach(function(checkbox) {
-            var item = checkbox.closest('.block-item__content__item');
-            const itemId = checkbox.dataset.id;
-            const isChecked = localStorage.getItem(itemId) === 'true';
-            checkbox.checked = isChecked;
-
-            if (isChecked) {
-                item.classList.add('checked');
-            } else {
-                item.classList.remove('checked');
+                const subtitleElement = document.createElement("div");
+                subtitleElement.className = "subtitle";
+                subtitleElement.textContent = item.subtitle;
+                itemElement.appendChild(subtitleElement);
             }
+
+            contentElement.appendChild(itemElement);
         });
+
+        checklistElement.appendChild(contentElement);
+
+        return checklistElement;
     }
 
-    document.getElementById('clearLocalStorageButton').addEventListener('click', function() {
-        localStorage.clear();
-        alert('Checklist is clear');
+    // Разделение данных на левую и правую колонки
+    const leftColumnData = checklistData.slice(0, Math.ceil(checklistData.length / 2));
+    const rightColumnData = checklistData.slice(Math.ceil(checklistData.length / 2));
+
+    // Добавление элементов в HTML
+    const leftColumn = document.getElementById("leftColumn");
+    leftColumnData.forEach(checklist => {
+        leftColumn.appendChild(createChecklistElement(checklist));
     });
 
-    loadChecklistState();
+    const rightColumn = document.getElementById("rightColumn");
+    rightColumnData.forEach(checklist => {
+        rightColumn.appendChild(createChecklistElement(checklist));
+    });
+
+
+    setTimeout(function (){
+
+        var checkboxes = document.querySelectorAll('.custom-checkbox input[type="checkbox"]');
+
+        checkboxes.forEach(function(checkbox){
+            checkbox.addEventListener('change', function() {
+                var blockItem = this.closest('.block-item__content__item');
+                if (this.checked) {
+                    blockItem.classList.add('checked');
+                } else {
+                    blockItem.classList.remove('checked');
+                }
+            });
+        });
+
+        const checklist = document.querySelectorAll('.custom-checkbox input[type="checkbox"]');
+
+        checklist.forEach(function(checkbox) {
+            checkbox.addEventListener('change', function(event) {
+                const itemId = event.target.dataset.id;
+                const isChecked = event.target.checked;
+                localStorage.setItem(itemId, isChecked);
+            });
+        });
+
+        // Загрузка состояний чекбоксов из local storage при загрузке страницы
+        function loadChecklistState() {
+            var checkboxes = document.querySelectorAll('.custom-checkbox input[type="checkbox"]');
+
+            checkboxes.forEach(function(checkbox) {
+                var item = checkbox.closest('.block-item__content__item');
+                const itemId = checkbox.dataset.id;
+                const isChecked = localStorage.getItem(itemId) === 'true';
+                checkbox.checked = isChecked;
+
+                if (isChecked) {
+                    item.classList.add('checked');
+                } else {
+                    item.classList.remove('checked');
+                }
+            });
+        }
+
+        document.getElementById('clearLocalStorageButton').addEventListener('click', function() {
+            localStorage.clear();
+
+            loadChecklistState();
+
+            alert('Checklist is clear');
+        });
+
+        loadChecklistState();
+    }, 1000)
 });
