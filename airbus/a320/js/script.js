98427933d5cfa400ae7a4c08a41e4ae9b01a0bc3
Newind <nickibelov@yandex.ru>
2023-12-18 21:37:46 +0500

    add new checklists

--- airbus/a320/js/script.js
+++ airbus/a320/js/script.js
@@ -3,86 +3,86 @@ document.addEventListener('DOMContentLoaded', function () {
         {
             title: "COCKPIT PREPARATION",
             items: [
-                {id: 1, label: "GEAR PING and COVERS", subtitle: "REMOVED", typeItem: "standard"},
-                {id: 2, label: "FUEL QUANTITY", subtitle: "___KG", typeItem: "standard"},
-                {id: 3, label: "SEAT BELTS", subtitle: "ON", typeItem: "standard"},
-                {id: 4, label: "ADIRS", subtitle: "NAV", typeItem: "standard"},
-                {id: 5, label: "BARO REF", subtitle: "____", typeItem: "standard"},
+                {id: 'a320-1', label: "GEAR PING and COVERS", subtitle: "REMOVED", typeItem: "standard"},
+                {id: 'a320-2', label: "FUEL QUANTITY", subtitle: "___KG", typeItem: "standard"},
+                {id: 'a320-3', label: "SEAT BELTS", subtitle: "ON", typeItem: "standard"},
+                {id: 'a320-4', label: "ADIRS", subtitle: "NAV", typeItem: "standard"},
+                {id: 'a320-5', label: "BARO REF", subtitle: "____", typeItem: "standard"},
 
             ]
         },
         {
             title: "BEFORE START",
             items: [
-                {id: 6, label: "PARKING BRAKE", subtitle: "SET", typeItem: "standard"},
-                {id: 7, label: "T.O. SPEEDS & THRUST", subtitle: "____", typeItem: "standard"},
-                {id: 8, label: "WINDOWS", subtitle: "CLOSED", typeItem: "standard"},
-                {id: 9, label: "BEACON", subtitle: "ON", typeItem: "standard"},
+                {id: 'a320-6', label: "PARKING BRAKE", subtitle: "SET", typeItem: "standard"},
+                {id: 'a320-7', label: "T.O. SPEEDS & THRUST", subtitle: "____", typeItem: "standard"},
+                {id: 'a320-8', label: "WINDOWS", subtitle: "CLOSED", typeItem: "standard"},
+                {id: 'a320-9', label: "BEACON", subtitle: "ON", typeItem: "standard"},
             ]
         },
         {
             title: "AFTER START",
             items: [
-                {id: 10, label: "ANTI ICE", subtitle: "____", typeItem: "standard"},
-                {id: 11, label: "ECAM STATUS", subtitle: "CHECKED", typeItem: "standard"},
-                {id: 12, label: "PITCH TRIM", subtitle: "__%", typeItem: "standard"},
-                {id: 13, label: "RUDDER TRIM", subtitle: "NEUTRAL", typeItem: "standard"},
+                {id: 'a320-10', label: "ANTI ICE", subtitle: "____", typeItem: "standard"},
+                {id: 'a320-11', label: "ECAM STATUS", subtitle: "CHECKED", typeItem: "standard"},
+                {id: 'a320-12', label: "PITCH TRIM", subtitle: "__%", typeItem: "standard"},
+                {id: 'a320-13', label: "RUDDER TRIM", subtitle: "NEUTRAL", typeItem: "standard"},
             ]
         },
         {
             title: "TAXI",
             items: [
-                {id: 14, label: "FLIGHT CONTROL", subtitle: "CHECKED", typeItem: "standard"},
-                {id: 15, label: "FLAPS SETTING", subtitle: "CONF __", typeItem: "standard"},
-                {id: 16, label: "RADAR & PRED W/S", subtitle: "ON & AUTO", typeItem: "standard"},
-                {id: 17, label: "ENG MODE SEL", subtitle: "____", typeItem: "standard"},
-                {id: 18, label: "ECAM MEMO", subtitle: "TO NO BLUE", typeItem: "standard"},
-                {id: 19, label: "ECAM MEMO", subtitle: "TO NO BLUE", typeItem: "standard"},
+                {id: 'a320-14', label: "FLIGHT CONTROL", subtitle: "CHECKED", typeItem: "standard"},
+                {id: 'a320-15', label: "FLAPS SETTING", subtitle: "CONF __", typeItem: "standard"},
+                {id: 'a320-16', label: "RADAR & PRED W/S", subtitle: "ON & AUTO", typeItem: "standard"},
+                {id: 'a320-17', label: "ENG MODE SEL", subtitle: "____", typeItem: "standard"},
+                {id: 'a320-18', label: "ECAM MEMO", subtitle: "TO NO BLUE", typeItem: "standard"},
+                {id: 'a320-19', label: "ECAM MEMO", subtitle: "TO NO BLUE", typeItem: "standard"},
 
-                {id: 20, label: "AUTO BRK MAX", subtitle: "", typeItem: "italic"},
-                {id: 21, label: "SIGNS ON", subtitle: "", typeItem: "italic"},
-                {id: 22, label: "CABIN READY", subtitle: "", typeItem: "italic"},
-                {id: 23, label: "SPLRS ARM", subtitle: "", typeItem: "italic"},
-                {id: 24, label: "FLAPS TO", subtitle: "", typeItem: "italic"},
-                {id: 25, label: "TO CONFIG NORM", subtitle: "", typeItem: "italic"},
+                {id: 'a320-20', label: "AUTO BRK MAX", subtitle: "", typeItem: "italic"},
+                {id: 'a320-21', label: "SIGNS ON", subtitle: "", typeItem: "italic"},
+                {id: 'a320-22', label: "CABIN READY", subtitle: "", typeItem: "italic"},
+                {id: 'a320-23', label: "SPLRS ARM", subtitle: "", typeItem: "italic"},
+                {id: 'a320-24', label: "FLAPS TO", subtitle: "", typeItem: "italic"},
+                {id: 'a320-25', label: "TO CONFIG NORM", subtitle: "", typeItem: "italic"},
             ]
         },
         {
             title: "LINE UP",
             items: [
-                {id: 27, label: "T.O RWY", subtitle: "___", typeItem: "standard"},
-                {id: 28, label: "TCAS", subtitle: "TA/RA", typeItem: "standard"},
-                {id: 29, label: "PACKS 1 & 2", subtitle: "___", typeItem: "standard"},
+                {id: 'a320-27', label: "T.O RWY", subtitle: "___", typeItem: "standard"},
+                {id: 'a320-28', label: "TCAS", subtitle: "TA/RA", typeItem: "standard"},
+                {id: 'a320-29', label: "PACKS 1 & 2", subtitle: "___", typeItem: "standard"},
             ]
         },
         {
             title: "<< DEPARTURE CHANGE >>",
             items: [
-                {id: 30, label: "RWY & SID", subtitle: "____", typeItem: "standard"},
-                {id: 31, label: "FLAPS SETTING", subtitle: "CONF____", typeItem: "standard"},
-                {id: 32, label: "T.0 SPEEDS & THRUST", subtitle: "___", typeItem: "standard"},
-                {id: 33, label: "FCU ALT", subtitle: "___", typeItem: "standard"},
+                {id: 'a320-30', label: "RWY & SID", subtitle: "____", typeItem: "standard"},
+                {id: 'a320-31', label: "FLAPS SETTING", subtitle: "CONF____", typeItem: "standard"},
+                {id: 'a320-32', label: "T.0 SPEEDS & THRUST", subtitle: "___", typeItem: "standard"},
+                {id: 'a320-33', label: "FCU ALT", subtitle: "___", typeItem: "standard"},
             ]
         },
         {
             title: "APPROACH",
             items: [
-                {id: 34, label: "BARO REF", subtitle: "____", typeItem: "standard"},
-                {id: 35, label: "SEAT BELTS", subtitle: "ON", typeItem: "standard"},
-                {id: 36, label: "MINIMUM", subtitle: "___", typeItem: "standard"},
-                {id: 37, label: "AUTO BRAKE", subtitle: "___", typeItem: "standard"},
-                {id: 38, label: "ENG MODE SEL", subtitle: "___", typeItem: "standard"},
+                {id: 'a320-34', label: "BARO REF", subtitle: "____", typeItem: "standard"},
+                {id: 'a320-35', label: "SEAT BELTS", subtitle: "ON", typeItem: "standard"},
+                {id: 'a320-36', label: "MINIMUM", subtitle: "___", typeItem: "standard"},
+                {id: 'a320-37', label: "AUTO BRAKE", subtitle: "___", typeItem: "standard"},
+                {id: 'a320-38', label: "ENG MODE SEL", subtitle: "___", typeItem: "standard"},
             ]
         },
         {
             title: "LANDING",
             items: [
-                {id: 39, label: "ECAM MEMO", subtitle: "LGD NO BLUE", typeItem: "standard"},
-                {id: 40, label: "LDG GEAR DN", subtitle: "ON", typeItem: "italic"},
-                {id: 41, label: "SIGNS ON", subtitle: "ON", typeItem: "italic"},
-                {id: 42, label: "CABIN READY", subtitle: "", typeItem: "italic"},
-                {id: 43, label: "SPLRS ARM", subtitle: "", typeItem: "italic"},
-                {id: 44, label: "FLAPS SET", subtitle: "", typeItem: "italic"},
+                {id: 'a320-39', label: "ECAM MEMO", subtitle: "LGD NO BLUE", typeItem: "standard"},
+                {id: 'a320-40', label: "LDG GEAR DN", subtitle: "ON", typeItem: "italic"},
+                {id: 'a320-41', label: "SIGNS ON", subtitle: "ON", typeItem: "italic"},
+                {id: 'a320-42', label: "CABIN READY", subtitle: "", typeItem: "italic"},
+                {id: 'a320-43', label: "SPLRS ARM", subtitle: "", typeItem: "italic"},
+                {id: 'a320-44', label: "FLAPS SET", subtitle: "", typeItem: "italic"},
             ]
         },
         {
@@ -94,19 +94,19 @@ document.addEventListener('DOMContentLoaded', function () {
         {
             title: "PARKING",
             items: [
-                {id: 46, label: "PARK BRK OR CHOCKS", subtitle: "SET", typeItem: "standard"},
-                {id: 47, label: "ENGINES", subtitle: "OFF", typeItem: "standard"},
-                {id: 48, label: "WING LIGHTS", subtitle: "OFF", typeItem: "standard"},
-                {id: 49, label: "FUEL PUPMS", subtitle: "OFF", typeItem: "standard"},
+                {id: 'a320-46', label: "PARK BRK OR CHOCKS", subtitle: "SET", typeItem: "standard"},
+                {id: 'a320-47', label: "ENGINES", subtitle: "OFF", typeItem: "standard"},
+                {id: 'a320-48', label: "WING LIGHTS", subtitle: "OFF", typeItem: "standard"},
+                {id: 'a320-49', label: "FUEL PUPMS", subtitle: "OFF", typeItem: "standard"},
             ]
         },
         {
             title: "SECURING THE AIRCRAFT",
             items: [
-                {id: 50, label: "OXYGEN", subtitle: "OFF", typeItem: "standard"},
-                {id: 51, label: "EMER EXIT LT", subtitle: "OFF", typeItem: "standard"},
-                {id: 52, label: "EFBs", subtitle: "OFF", typeItem: "standard"},
-                {id: 53, label: "BATTERIES", subtitle: "OFF", typeItem: "standard"},
+                {id: 'a320-50', label: "OXYGEN", subtitle: "OFF", typeItem: "standard"},
+                {id: 'a320-51', label: "EMER EXIT LT", subtitle: "OFF", typeItem: "standard"},
+                {id: 'a320-52', label: "EFBs", subtitle: "OFF", typeItem: "standard"},
+                {id: 'a320-53', label: "BATTERIES", subtitle: "OFF", typeItem: "standard"},
             ]
         },
     ];
